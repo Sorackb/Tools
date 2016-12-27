17f3a5c8fd4d09d749dfbf6ae383aa5570367688
Sorackb <sorackb@gmail.com>
2016-12-27 11:16:03 -0200

    Usability improvement

--- src/main/java/org/lucassouza/tools/MessageType.java
+++ src/main/java/org/lucassouza/tools/MessageType.java
@@ -5,5 +5,5 @@ package org.lucassouza.tools;
  * @author Lucas Souza [sorackb@gmail.com]
  */
 public enum MessageType {
-  ERROR, WARN, LOG, INFO
+  ERROR, WARN, LOG, INFO, START_TIMER, FINISH_TIMER;
 }
